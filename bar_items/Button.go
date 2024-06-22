8e56788147f7b95238cd19b4730fe53ef2e62fdd
Mauricio Mahmud Sanchez <mauxms1996@gmail.com>
2024-06-22 21:58:08 +0200

    load the image as texture once
    - testConfig updated
    - add test icon images

--- bar_items/Button.go
+++ bar_items/Button.go
@@ -9,33 +9,37 @@ import (
 
 type Button struct {
 	sdl.Rect
-	imgPath string
-	action  func(ctx *BarContext)
+	label        string
+	imgAsSurface *sdl.Surface
+	action       func(ctx *BarContext)
 }
 
 func NewButton(W, H int32, imgPath string, action func(ctx *BarContext)) *Button {
-	return &Button{
-		Rect: sdl.Rect{
-			W: W,
-			H: H,
-		},
-		imgPath: imgPath,
-		action:  action,
-	}
-}
-
-func (butt *Button) Draw(surf *sdl.Surface, bar *BarContext) {
-	rw := sdl.RWFromFile(butt.imgPath, "r")
-	icon, err := sdlImg.LoadPNGRW(rw)
-	if butt == bar.HoveredItem {
-		icon.SetAlphaMod(126)
-	}
+	rw := sdl.RWFromFile(imgPath, "r")
+	img, err := sdlImg.LoadPNGRW(rw)
 
 	if err != nil {
 		log.Printf("Error loading the button Image (%s)\n", err.Error())
 	}
 
-	icon.BlitScaled(nil, surf, &sdl.Rect{
+	return &Button{
+		Rect: sdl.Rect{
+			W: W,
+			H: H,
+		},
+		imgAsSurface: img,
+		action:       action,
+	}
+}
+
+func (butt *Button) Draw(surf *sdl.Surface, bar *BarContext) {
+	if butt == bar.HoveredItem {
+		butt.imgAsSurface.SetAlphaMod(126)
+	} else {
+		butt.imgAsSurface.SetAlphaMod(255)
+	}
+
+	butt.imgAsSurface.BlitScaled(nil, surf, &sdl.Rect{
 		X: butt.X,
 		Y: butt.Y,
 		W: int32(butt.W),

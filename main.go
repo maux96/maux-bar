8a1f8c8e35ca9acca384c89f3702219b5b48a7f8
Mauricio Mahmud Sanchez <mauxms1996@gmail.com>
2024-06-26 19:34:01 +0200

    select config file from args

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"log"
+	"flag"
+
 	"maux_bar/bar_items"
 	"maux_bar/bar_visual"
 	"maux_bar/config_loader"
 )
 
 func main() {
+	flag.Parse()
+
+	var configFilePath string 
+	if flag.NArg() == 0 {
+		log.Fatalln("JSON config file needed as first argument.")
+	} else {
+		configFilePath = flag.Arg(0)
+	}
+
 	bar := bar_items.NewBarContext()
-	config_loader.PrepareBar(bar, "./testConfig.json")
+	err := config_loader.PrepareBar(bar, configFilePath)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 
 	QuitFunction := bar_visual.InitEverythingSDL()
 	defer QuitFunction()
